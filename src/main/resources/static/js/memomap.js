f8ae7594e0a14051bc0a0f071c4b7d20be51f0b4
seonjin <57834016+hingkop@users.noreply.github.com>
2024-04-22 11:19:41 +0900

    feat: 메인 완성(중심 좌표 수정 요청)

--- src/main/resources/static/js/memomap.js
+++ src/main/resources/static/js/memomap.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var container = document.getElementById('map');
-    var options = {
-        center: new kakao.maps.LatLng(37.58163301627676, 126.8860228466823),
+    var mapContainer = document.getElementById('map');
+    var mapOptions = {
+        center: new kakao.maps.LatLng(37.58163301627676, 126.8860228466823), // 올바른 표기로 수정
         level: 3
     };
 
-    var map = new kakao.maps.Map(container, options);
+    var map = new kakao.maps.Map(mapContainer, mapOptions);
+
     var marker = new kakao.maps.Marker({
         position: map.getCenter()
     });
     marker.setMap(map);
 
     var currentLat, currentLng;
+
 
     kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
         var latlng = mouseEvent.latLng;
@@ -38,47 +40,49 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             alert("Geolocation is not supported by this browser.");
         }
-    }
+    };
 
     var positions = [
         {
-            content: '<div>카카오</div>',
-            latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+            content: '<div>상암 IT 타워</div>',
+            latlng: new kakao.maps.LatLng(37.58163301627676, 126.8860228466823)
+        },
+        {
+            content: '<div>LG 유플러스</div>',
+            latlng: new kakao.maps.LatLng(37.58057831675249, 126.88781319354182)
+        },
+        {
+            content: '<div>가온 문화 공원</div>',
+            latlng: new kakao.maps.LatLng(37.58191615523997, 126.88766966937938)
         }
+
     ];
 
-    for (var i = 0; i < positions.length; i ++) {
-        // 마커를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        map: map
+    });
+
+    positions.forEach(function(pos) {
         var marker = new kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: positions[i].latlng // 마커의 위치
+            map: map,
+            position: pos.latlng
         });
 
-        // 마커에 표시할 인포윈도우를 생성합니다
-        var infowindow = new kakao.maps.InfoWindow({
-            content: positions[i].content // 인포윈도우에 표시할 내용
-        });
-
-        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-        // 이벤트 리스너로는 클로저를 만들어 등록합니다
-        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-        kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-        kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-    }
-
-// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-    function makeOverListener(map, marker, infowindow) {
-        return function() {
+        kakao.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(pos.content);
             infowindow.open(map, marker);
-        };
-    }
+        });
 
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다
-    function makeOutListener(infowindow) {
-        return function() {
-            infowindow.close();
-        };
-    }
+        infowindow.close();
+
+    });
+
+    // 2. DOMContentLoaded 이벤트 핸들러 위치 확인하기
+    console.log('DOMContentLoaded 이벤트 핸들러가 실행되었습니다.');
+
+    // 3. kakao.maps.LatLng에 전달되는 값을 확인하기
+    console.log('LatLng 확인:', positions.map(pos => [pos.latlng.getLat(), pos.latlng.getLng()]));
+
 
 
 });
