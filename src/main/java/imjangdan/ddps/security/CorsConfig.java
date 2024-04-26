c28c5d01077db4740c7403409e2cef0537c9c2ac
Huiju Suh <70363184+heeeej02@users.noreply.github.com>
2024-04-26 14:53:19 +0900

    fix : CorsConfig 변경

--- src/main/java/imjangdan/ddps/security/CorsConfig.java
+++ src/main/java/imjangdan/ddps/security/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PATCH", "DELETE", "PUT"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
 
